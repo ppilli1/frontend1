d3b08e108344e0ad0efe48f55f2f4dcdd8d4ea13
Pratham Pilli <67479423+ppilli1@users.noreply.github.com>
2023-11-02 21:38:25 -0400

    Add files via upload
    Updated Routes

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -5,9 +5,11 @@ import Signup from "../pages/Signup"
 import Contact from "../pages/Contact"
 import Doctors from "../pages/Doctors/Doctors"
 import DoctorDetails from "../pages/Doctors/DoctorDetails"
+import Chatbot from "../pages/Chatbot"
+import Practice from "../../Practice"
+
 
 import {Routes, Route} from "react-router-dom"
-
 const Routers = () => {
   return <Routes>
     <Route path = "/" element = {<Home/>}/>
@@ -18,6 +20,8 @@ const Routers = () => {
     <Route path = "/register" element = {<Signup/>}/>
     <Route path = "/contact" element = {<Contact/>}/>
     <Route path = "/services" element = {<Services/>}/>
+    <Route path = "/chatbot" element = {<Chatbot/>}/>
+    <Route path = "/practice" element = {<Practice/>}/>
   </Routes>
 }
 
